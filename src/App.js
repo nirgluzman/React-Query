bd57f92941beaea00a93cafcbe24e31e0798ad8b
nirgluzman <nir.gluzman@gmail.com>
2023-05-23 00:54:05 +0200

    add useMutation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
 
 import './App.css';
 
 function App() {
   const [id, setId] = useState();
+  const queryClient = useQueryClient();
 
   const {
     data: results,
@@ -15,15 +16,26 @@ function App() {
     queryKey: ['products'],
     queryFn: async () =>
       await axios.get('https://api.escuelajs.co/api/v1/products'),
-    staleTime: 1000 * 60 * 10, // the length of time before the data becomes stale (i.e. max time to be considered fresh)
+    staleTime: 1000 * 60 * 10, // the length of time before the data becomes stale (i.e. max time to be considered fresh).
   });
 
   const { data: productResult } = useQuery({
     queryKey: [`product${id}`],
     queryFn: async () =>
       await axios.get(`https://api.escuelajs.co/api/v1/products/${id}`),
-    enabled: !!id, // to control when to execute the fetch query
+    enabled: !!id, // to control when to execute the fetch query.
     staleTime: 1000 * 60 * 10,
+  });
+
+  const mutation = useMutation({
+    mutationFn: async (body) =>
+      await axios.put('https://api.escuelajs.co/api/v1/products/8', body),
+    onSuccess: () => {
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: ['products'] }); // marks queries as stale and overrides any staleTime configurations.
+        queryClient.invalidateQueries({ queryKey: [`product${id}`] });
+      }, 1000);
+    },
   });
 
   if (isLoading) {
@@ -37,6 +49,9 @@ function App() {
   return (
     <>
       <h1>{productResult?.data.title}</h1>
+      <button onClick={() => mutation.mutate({ title: 'Hello World' })}>
+        Update item
+      </button>
       <ul>
         {results?.data.map((item) => (
           <li key={item.id} onClick={() => setId(item.id)}>
