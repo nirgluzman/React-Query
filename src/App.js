eeeb485142f28edc5503da89dfdc260eb6606893
nirgluzman <nir.gluzman@gmail.com>
2023-05-23 00:26:03 +0200

    add useQuery for productResult

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
 import './App.css';
 
 function App() {
+  const [id, setId] = useState();
+
   const {
     data: results,
     isLoading,
@@ -13,6 +16,14 @@ function App() {
     queryFn: async () =>
       await axios.get('https://api.escuelajs.co/api/v1/products'),
     staleTime: 1000 * 60 * 10, // the length of time before the data becomes stale (i.e. max time to be considered fresh)
+  });
+
+  const { data: productResult } = useQuery({
+    queryKey: [`product${id}`],
+    queryFn: async () =>
+      await axios.get(`https://api.escuelajs.co/api/v1/products/${id}`),
+    enabled: !!id, // to control when to execute the fetch query
+    staleTime: 1000 * 60 * 10,
   });
 
   if (isLoading) {
@@ -25,9 +36,12 @@ function App() {
 
   return (
     <>
+      <h1>{productResult?.data.title}</h1>
       <ul>
         {results?.data.map((item) => (
-          <li key={item.id}>{item.title}</li>
+          <li key={item.id} onClick={() => setId(item.id)}>
+            {item.title}
+          </li>
         ))}
       </ul>
     </>
